05b33303272d5bc4f073e3490f45e3df239ff57d
ShriyaliPahariya <shriyali1999@gmail.com>
2019-12-02 03:46:36 +0530

    version3

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/index', function(req, res, next)  {
+  res.render('dashboard/index', {
+
+  });
+})
+
+router.get('/course', function(req, res, next) {
+  res.render('dashboard/courses');
+})
+
+router.get('/quiz', function(req, res, next) {
+  res.render('dashboard/testquiz');
+})
+
 module.exports = router;
